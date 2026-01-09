bfab203038d8118ddd0a635fda7cb4a12bd3c33c
esgg <eg.guardia@gmail.com>
2026-01-09 09:15:04 +0100

    Algorithm to process RO added

---
+++ code/fair_assessment/algorithms/FAIROS_RESEARCH_OBJECT.py
@@ -0,0 +1,12 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+class FAIROS_RESEARCH_OBJECT:
+    def execute_algorithm(rocrate_content):
+        logging.info("Assessing Research Object information")
+
+    def get_id():
+        return "FAIROS_RESEARCH_OBJECT"
+    def getTTL():
+        return "simple"

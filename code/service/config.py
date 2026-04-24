5050b6eb11296a1cecac1e21bf81bbebeaf8e960
esgg <eg.guardia@gmail.com>
2026-04-24 11:40:39 +0200

    SCORE_DIRECTORY variable added

--- code/service/config.py
+++ code/service/config.py
@@ -5,6 +5,11 @@ class Settings(BaseSettings):
     FILE_STORAGE_DIR: Path
     NO_TOKEN_DIRECTORY: Path
     METRICS_DIRECTORY: Path
+    TESTS_DIRECTORY: Path
+    BENCHMARKS_DIRECTORY: Path
+    SCORE_DIRECTORY: Path
+    MQTT_HOST: str
+    MQTT_PORT: int
 
     class Config:
         env_file = ".env"
